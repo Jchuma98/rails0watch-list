0051d74295176d8c9aa602bbca214b599e0761df
Joaquin Chumaceiro <cajoa@yahoo.com>
2022-01-22 14:30:47 +0100

    propblem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  def show
+    @movies = Movies.all
+  end
+
+  def show_specific
+    @specific_movie = Movies.find(params[:id])
+
+  end
+
+  def new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
-Rails.application.routes.draw do
+Rails.application..draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #get all lists
+  GET "lists", to: "application#show"
+  #get a specific list
+  GET "lists/:id", to: "application#show_specific"
+  #new list
+  GET "lists/new", to: "application#new"
+  POST "lists"
 end
